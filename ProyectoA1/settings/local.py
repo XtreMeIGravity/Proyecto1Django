6ea238be8507d0a9a15cfd78583b92cf0810dd40
XtreMeIGravity <xtremeigraviti@hotmail.com>
2020-12-09 03:10:40 -0600

    Subida1xD

--- ProyectoA1/settings/local.py
+++ ProyectoA1/settings/local.py
@@ -15,8 +15,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DB_Empleado',
+        'USER': 'UserEmpleado',
+        'PASSWORD': 'rootEmpleado',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
--- applications/empleado/models.py
+++ applications/empleado/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+from applications.departamento.models import Departamento
+
+
 # Create your models here.
+
+class Empleado(models.Model):
+
+    Job_Choices = (
+        ('0', 'Contador'),
+        ('1', 'Administrador'),
+        ('2', 'Economista'),
+        ('3', 'Otro'),
+    )
+
+    firts_name = models.CharField('Nombres', max_length=50)
+    last_name = models.CharField('Apellidos', max_length=50)
+    job = models.CharField('Trabajo', max_length=1, choices=Job_Choices)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+    #
+
+    class Meta:
+        verbose_name = "Empleado"
+        verbose_name_plural = "Empleados"
+
+    def __str__(self):
+        return self.firts_name +' - '+ self.last_name
