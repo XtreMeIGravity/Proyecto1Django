355501136253b700e2493417d242f897f353285d
XtreMeiGravity <xtremeigraviti@hotmail.com>
2021-08-26 15:55:23 -0500

    Subida1xD

---
+++ ProyectoA1/settings/prod.py
@@ -0,0 +1,33 @@
+"""
+
+"""
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_empleado',
+        'USER': 'userempleado',
+        'PASSWORD': 'rootEmpleado',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
